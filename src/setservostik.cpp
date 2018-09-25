1d7f7a42c8335135961bf01a744e4bc7551a4503
Gijs <dwg@telenet.be>
2018-09-25 21:18:57 +0200

    some code code cleanup

--- src/setservostik.cpp
+++ src/setservostik.cpp
@@ -32,17 +32,16 @@
 #define KERNEL_DRIVER_ATTACHED 1
 #define VERSION "1.0.1"
 
-auto getdevice(libusb_context *&context, const unsigned int &vendor, const unsigned int &product, libusb_device *&device){
+auto getdevice(libusb_context *&context, const unsigned int &vendor, const unsigned int &product, libusb_device *&device) {
     libusb_device **devices;
     for (auto idx(0); idx < libusb_get_device_list(context, &devices); idx++) {
         device = devices[idx];
         libusb_device_descriptor descriptor = {0};
         auto rc = libusb_get_device_descriptor(device, &descriptor);
-        if(rc != LIBUSB_SUCCESS) {
-            std::cout << "WARNING: " << libusb_error_name(rc) << " - " << libusb_strerror((libusb_error) rc)  << " - trying to proceed...\n";
-        }
-        else{
-            if((descriptor.idVendor==vendor)&&(descriptor.idProduct==product)){
+        if (rc != LIBUSB_SUCCESS) {
+            std::cout << "WARNING: " << libusb_error_name(rc) << " - " << libusb_strerror((libusb_error) rc) << " - trying to proceed...\n";
+        } else {
+            if ((descriptor.idVendor == vendor) && (descriptor.idProduct == product)) {
                 libusb_free_device_list(devices, 1);
                 return true;
             }
@@ -52,19 +51,19 @@ auto getdevice(libusb_context *&context, const unsigned int &vendor, const unsig
     return false;
 }
 
-void cleanup(libusb_context *&context, libusb_device_handle *&devicehandle){
-    if(devicehandle) { libusb_close(devicehandle); }
-    if(context) { libusb_exit(context); }
+void cleanup(libusb_context *&context, libusb_device_handle *&devicehandle) {
+    if (devicehandle) { libusb_close(devicehandle); }
+    if (context) { libusb_exit(context); }
 }
 
-void errorhandler(libusb_context *&context, libusb_device_handle *&devicehandle, const std::string &errormessage){
-    std::cerr << "ERROR: " << errormessage  << "\n";
+void errorhandler(libusb_context *&context, libusb_device_handle *&devicehandle, const std::string &errormessage) {
+    std::cerr << "ERROR: " << errormessage << "\n";
     cleanup(context, devicehandle);
     exit(EXIT_FAILURE);
 }
 
-void errorhandler(libusb_context *&context, libusb_device_handle *&devicehandle, int rc){
-    std::cerr << "ERROR: " << libusb_error_name(rc) << " - " << libusb_strerror((libusb_error) rc)  << "\n";
+void errorhandler(libusb_context *&context, libusb_device_handle *&devicehandle, int rc) {
+    std::cerr << "ERROR: " << libusb_error_name(rc) << " - " << libusb_strerror((libusb_error) rc) << "\n";
     cleanup(context, devicehandle);
     exit(EXIT_FAILURE);
 }
@@ -76,26 +75,25 @@ auto rotatestick(int way) {
     auto rc(0);
 
     rc = libusb_init(&context);
-    if(rc != LIBUSB_SUCCESS) { errorhandler(context, devicehandle, rc); }
-    libusb_set_debug(context, LIBUSB_LOG_LEVEL_INFO );
+    if (rc != LIBUSB_SUCCESS) { errorhandler(context, devicehandle, rc); }
+    libusb_set_debug(context, LIBUSB_LOG_LEVEL_INFO);
 
-    if(!getdevice(context,SERVOSTIK_VENDOR,SERVOSTIK_PRODUCT,device)){
+    if (!getdevice(context, SERVOSTIK_VENDOR, SERVOSTIK_PRODUCT, device)) {
         std::stringstream ss;
         ss << std::hex << "0x" << SERVOSTIK_VENDOR << ":0x" << std::hex << SERVOSTIK_PRODUCT << " device not found";
-        errorhandler(context,devicehandle,ss.str());
-    }
-    else{ //good to go
+        errorhandler(context, devicehandle, ss.str());
+    } else { //good to go
         rc = libusb_open(device, &devicehandle);
-        if(rc != LIBUSB_SUCCESS) { errorhandler(context, devicehandle, rc); }
-        if(devicehandle){
+        if (rc != LIBUSB_SUCCESS) { errorhandler(context, devicehandle, rc); }
+        if (devicehandle) {
             if (libusb_kernel_driver_active(devicehandle, SERVOSTIK_INTERFACE) == KERNEL_DRIVER_ATTACHED) {
                 rc = libusb_detach_kernel_driver(devicehandle, SERVOSTIK_INTERFACE);
                 if (rc != LIBUSB_SUCCESS) { errorhandler(context, devicehandle, rc); }
             }
             rc = libusb_claim_interface(devicehandle, SERVOSTIK_INTERFACE);
-            if(rc != LIBUSB_SUCCESS) { errorhandler(context, devicehandle, rc); }
-            unsigned char message[SERVOSTIK_MESG_LENGTH] = {0x00,0x00,0x00,0x00}; //4-way
-            if(way==8) { message[3] = 0x01; } //8-way
+            if (rc != LIBUSB_SUCCESS) { errorhandler(context, devicehandle, rc); }
+            unsigned char message[SERVOSTIK_MESG_LENGTH] = {0x00, 0x00, 0x00, 0x00}; //4-way
+            if (way == 8) { message[3] = 0x01; } //8-way
             rc = libusb_control_transfer(devicehandle, UM_REQUEST_TYPE, UM_REQUEST, SERVOSTIK_VALUE, SERVOSTIK_INTERFACE, message, SERVOSTIK_MESG_LENGTH, UM_TIMEOUT);
             std::cout << "servostik " << way << "-way -> " << ((rc == sizeof(message)) ? "SUCCESS" : "FAILURE") << "\n";
             rc = libusb_release_interface(devicehandle, SERVOSTIK_INTERFACE);
@@ -106,24 +104,23 @@ auto rotatestick(int way) {
     return EXIT_SUCCESS;
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
     long int way(8);
     switch (argc) {
         case 2:
-            try { way = std::stol(argv[1],nullptr,10); }
-            catch(std::exception &) { way = 8; }
+            try { way = std::stol(argv[1], nullptr, 10); }
+            catch (std::exception &) { way = 8; }
             break;
         default:
             std::cout
-            << " _____     _   _____                 _____ _   _ _    \n"
-            << "|   __|___| |_|   __|___ ___ _ _ ___|   __| |_|_| |_  \n"
-            << "|__   | -_|  _|__   | -_|  _| | | . |__   |  _| | '_| \n"
-            << "|_____|___|_| |_____|___|_|  \\_/|___|_____|_| |_|_,_| \n"
-            << "setservostik Copyright (C) 2018  De Waegeneer Gijsbrecht\n"
-            << "Ultimarc ServoStik switcher Version "<< VERSION << "\n\n[ " << argv[0] << " restrictorway ] change joystick(s) to 4 or 8 way\n\n"
-            << "This program comes with ABSOLUTELY NO WARRANTY. This is free software,\nand you are welcome to redistribute it under certain conditions.\n"
-            << "license: GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007\nCopyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>\n";
+                    << " _____     _   _____                 _____ _   _ _    \n"
+                    << "|   __|___| |_|   __|___ ___ _ _ ___|   __| |_|_| |_  \n"
+                    << "|__   | -_|  _|__   | -_|  _| | | . |__   |  _| | '_| \n"
+                    << "|_____|___|_| |_____|___|_|  \\_/|___|_____|_| |_|_,_| \n"
+                    << "setservostik Copyright (C) 2018  De Waegeneer Gijsbrecht\n"
+                    << "Ultimarc ServoStik switcher Version " << VERSION << "\n\n[ " << argv[0] << " restrictorway ] change joystick(s) to 4 or 8 way\n\n"
+                    << "This program comes with ABSOLUTELY NO WARRANTY. This is free software,\nand you are welcome to redistribute it under certain conditions.\n"
+                    << "license: GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007\nCopyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>\n";
             return EXIT_SUCCESS;
     }
     return rotatestick((!((way > 0) && (way < 5))) ? 8 : 4);
